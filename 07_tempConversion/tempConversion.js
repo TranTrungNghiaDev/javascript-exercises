0eccb7e581ace94f977f3f3d23bfedfa5147caf1
TranTrungNghiaDev <trantrungnghiadev1998@gmail.com>
2024-06-20 10:45:51 +0700

    Change files.js, files.spece.js to learn basic js
    I learned how to round a number to 1 decimal place in JavaScript
    And how to find a leap years

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let result = (fahrenheit - 32) / 1.8;
+  result = Math.round(result * 10) / 10;
+  
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = celsius * 1.8 + 32;
+  result = Math.round(result * 10) / 10;
+
+  return result;
 };
 
 // Do not edit below this line
